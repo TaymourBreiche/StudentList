c2ca85bb15509928ed065b25f202dbd7f5e6f9a7
Taymour Breiche <breichet@sheridancollege.ca>
2023-05-16 09:59:44 -0400

    First Commit

---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,20 @@
+package studentlist;
+
+public class StudentList {
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Taymour", "s1");
+
+        Student[] studentList = new Student[3];
+        studentList[0] =s1;
+        studentList[1] = new Student("Mayank", "s2");
+        studentList[2] = new Student("Stephanie", "s3");
+
+        for (int i = 0; i < studentList.length; i++) {
+            System.out.println(studentList[i].getSname());
+            System.out.println(studentList[i].getsID());
+        }
+    }
+}
+    
+
