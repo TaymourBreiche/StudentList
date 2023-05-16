5ff37ca496916a84e712805b54d9a805b56c94dd
Taymour Breiche <breichet@sheridancollege.ca>
2023-05-16 10:07:41 -0400

    Added comment on student list and changed a students name

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -1,13 +1,13 @@
 package studentlist;
 
 public class StudentList {
-
+    //hello this is commit
     public static void main(String[] args) {
         Student s1 = new Student("Taymour", "s1");
 
         Student[] studentList = new Student[3];
         studentList[0] =s1;
-        studentList[1] = new Student("Mayank", "s2");
+        studentList[1] = new Student("Hadi", "s2");
         studentList[2] = new Student("Stephanie", "s3");
 
         for (int i = 0; i < studentList.length; i++) {
